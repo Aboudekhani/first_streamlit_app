f2e79683a3d14e6af92aa912f421d821965ffdee
aboudekhani <aboudekhani@gmail.com>
2023-10-13 18:32:37 -0400

    add second function

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,16 +35,23 @@ try:
         streamlit.dataframe(back_from_func)
 except URLError as e:
     streamlit.erro()
-streamlit.stop()
+
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("the fruit list contain: ")
-streamlit.dataframe(my_data_rows)
 
+streamlit.header("the fruit list contain: ")
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+        return my_cur.fetchall() 
+
+
+if streamlit.button('Get fruit list'):
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
+streamlit.stop()
 add_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for jack box ', add_fruit) 
 
