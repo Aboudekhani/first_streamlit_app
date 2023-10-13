4d32c719ea452421bf0f1bc3af4c1b9a02470e5f
Karim <56273354+Aboudekhani@users.noreply.github.com>
2023-10-13 17:24:13 -0400

    add 2nd input

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("the fruit list contain: ")
 streamlit.dataframe(my_data_rows)
 
+add_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
+streamlit.write('Thanks for jack box ', fruit_choice) 
+
