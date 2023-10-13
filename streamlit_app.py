8790415fd094bc76c50521fe1daa2ba569272159
Karim <56273354+Aboudekhani@users.noreply.github.com>
2023-10-13 00:30:02 -0400

    add fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.text("the fruit list contain: ")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
