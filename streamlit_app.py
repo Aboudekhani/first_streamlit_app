64e6bfcf6f975f4805c95dded902f003b7eaa95d
Karim <56273354+Aboudekhani@users.noreply.github.com>
2023-10-13 17:51:04 -0400

    add query

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,4 @@ streamlit.dataframe(my_data_rows)
 add_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for jack box ', add_fruit) 
 
+my_cur.execute('insert into fruit_load_list values(\'from streamlit\')')
